f355ea2fe1d58cd1a728ed8659065d3af9c62a6b
rdwj <wjackson@redhat.com>
2025-07-25 09:10:51 -0500

    fixed imports and re tested

--- test-nomic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Test the Nomic Embed API
-curl -X POST https://nomic-embed-text-v1-5-maas-apicast-production.apps.prod.rhoai.rh-aiservices-bu.com:443/v1/embeddings \
-  -H "Authorization: Bearer b149099203fe842343682b36c333f1c8" \
-  -H "Content-Type: application/json" \
-  -d '{"model": "nomic-embed-text", "input": "Hello, world!"}'
